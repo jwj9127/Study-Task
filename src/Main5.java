85f8844ad1836eafd4855a000bf1dad0d31c8011
jwj <jwj9127@naver.com>
2023-06-26 00:41:44 +0900

    백준 25314 문제 알고리즘

---
+++ src/Main5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main5 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        N = N / 4;
+
+        String string = "long";    // long을 문자열에 넣고
+        String str = " long";
+
+        for(int i = 1; i < N; i++){ // N번 반복
+            string = string + str;       // 반복 횟수에 따른 long 문자열 더하기
+        }
+        System.out.println(string + " int");
+
+    }
+}
