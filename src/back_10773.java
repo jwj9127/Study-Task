a01cb430cc6d223a3a378d05a2705aa71f290a36
jwj <jwj9127@naver.com>
2023-07-07 01:14:51 +0900

    back_10773

--- src/back_10773.java
+++ src/back_10773.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Stack;
 
-public class Main {
+public class back_10773 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
